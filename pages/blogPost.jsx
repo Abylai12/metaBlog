3e0338c110b1335dd22e5f68b8f5dd09b978ec94
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-08-05 00:16:58 +0800

    first commit

---
+++ pages/blogPost.jsx
@@ -0,0 +1,8 @@
+import LatestPost from "@/components/latesPost/latestPost";
+export default function BlogPost() {
+  return (
+    <section>
+      <LatestPost style="hidden" />
+    </section>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import LatestPost from "@/components/latesPost/latestPost";
+import SlayCard from "@/components/slayCard/slayCard";
+import TrendingPost from "@/components/trendingPost";
+
+export default function Home() {
+  return (
+    <section>
+      <SlayCard />
+      <TrendingPost />
+      <LatestPost />
+    </section>
+  );
+}
