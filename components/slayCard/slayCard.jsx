d87fa2f5e6ee4d9c6065c783bdd4bed23223f80e
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-11-20 12:33:01 +0800

    trending post and carousel done

--- components/slayCard/slayCard.jsx
+++ components/slayCard/slayCard.jsx
@@ -1,3 +1,85 @@
+// import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+// import TextCard from "./textCard";
+// import { useContext, useState, useEffect } from "react";
+// import { MyContext } from "@/context/context";
+// import Link from "next/link";
+// import { toast } from "react-toastify";
+// import Skeleton from "../loader/skeleton";
+
+// const SlayCard = () => {
+//   const { searchValue } = useContext(MyContext);
+//   const [articles, setArticles] = useState([]);
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const getLatestArticles = async () => {
+//     try {
+//       setIsLoading(true);
+//       const res = await fetch(`https://dev.to/api/articles?latest&per_page=5`);
+//       const data = await res.json();
+//       setArticles(data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       setIsLoading(false);
+//       toast.warning("Интернет холболтоо шалгана уу");
+//     }
+//   };
+//   useEffect(() => {
+//     getLatestArticles();
+//   }, []);
+
+//   const moveRight = () => {
+//     setCurrentIndex(() =>
+//       currentIndex < articles.length - 1 ? currentIndex + 1 : 0
+//     );
+//   };
+
+//   const moveLeft = () => {
+//     setCurrentIndex(() =>
+//       currentIndex > 0 ? currentIndex - 1 : articles.length - 1
+//     );
+//   };
+//   return (
+//     <section
+//       className={`${!searchValue ? "" : "hidden"} max-w-[1220px] m-auto `}
+//     >
+//       <div className=" w-[1220px] h-[600px] overflow-scroll p-5">
+//         {articles.map(
+//           ({ id, cover_image, title, readable_publish_date, type_of }, idx) => (
+//             <Link href={"/blog/" + id} key={id} currentIndex={idx}>
+//               <div
+//                 className=" w-full h-full bg-center bg-cover gradient relative rounded-xl"
+//                 style={{
+//                   backgroundImage: `url(${
+//                     cover_image ? cover_image : "./images/Union.png"
+//                   } )`,
+//                 }}
+//               >
+//                 <TextCard
+//                   articleTitle={title}
+//                   publishedDate={readable_publish_date}
+//                   articleType={type_of}
+//                 />
+//               </div>
+//               {isLoading && <Skeleton />}
+//             </Link>
+//           )
+//         )}
+//       </div>
+
+//       <div className="text-right mt-4">
+//         <button onClick={moveLeft} className="text-4xl mr-2">
+//           <FaArrowAltCircleLeft />
+//         </button>
+//         <button onClick={moveRight} className="text-4xl ">
+//           <FaArrowAltCircleRight />
+//         </button>
+//       </div>
+//     </section>
+//   );
+// };
+// export default SlayCard;
+
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 import TextCard from "./textCard";
 import { useContext, useState, useEffect } from "react";
@@ -27,9 +109,9 @@ const SlayCard = () => {
   }, []);
 
   const moveRight = () => {
-    // setCurrentIndex(() =>
-    //   currentIndex < articles.length - 1 ? currentIndex + 1 : 0
-    // );
+    setCurrentIndex(() =>
+      currentIndex < articles.length - 1 ? currentIndex + 1 : 0
+    );
   };
 
   const moveLeft = () => {
@@ -37,32 +119,39 @@ const SlayCard = () => {
       currentIndex > 0 ? currentIndex - 1 : articles.length - 1
     );
   };
+
   return (
     <section
       className={`${!searchValue ? "" : "hidden"} max-w-[1220px] m-auto `}
     >
-      <div className="flex overflow-scroll p-5">
-        {articles.map(
-          ({ id, cover_image, title, readable_publish_date, type_of }) => (
-            <Link href={"/blog/" + id} key={id}>
+      <div className="w-[1220px] h-[600px] overflow-hidden ">
+        <div
+          className=" flex h-full w-full transition-all ease-out duration-150"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {articles.map(
+            ({ id, cover_image, title, readable_publish_date, type_of }) => (
               <div
-                className="w-[1220px] h-[600px] bg-center bg-cover gradient relative rounded-xl"
+                className=" h-full min-w-full bg-center bg-cover gradient relative rounded-xl"
+                key={id}
                 style={{
                   backgroundImage: `url(${
                     cover_image ? cover_image : "./images/Union.png"
                   } )`,
                 }}
               >
-                <TextCard
-                  articleTitle={title}
-                  publishedDate={readable_publish_date}
-                  articleType={type_of}
-                />
+                <Link href={"/blog/" + id}>
+                  <TextCard
+                    articleTitle={title}
+                    publishedDate={readable_publish_date}
+                    articleType={type_of}
+                  />
+                </Link>
               </div>
-              {isLoading && <Skeleton />}
-            </Link>
-          )
-        )}
+            )
+          )}
+          {isLoading && <Skeleton />}
+        </div>
       </div>
 
       <div className="text-right mt-4">
--- components/trendingPost/index.jsx
+++ components/trendingPost/index.jsx
@@ -1,17 +1,68 @@
+// import TrendPostCard from "./tPostCard";
+// import { useState, useEffect, useContext } from "react";
+// import { MyContext } from "@/context/context";
+// import { toast } from "react-toastify";
+// import Loader from "../loader/loader";
+
+// const TrendingPost = () => {
+//   const { searchValue } = useContext(MyContext);
+//   const [trendArticle, setTrendArticle] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const trendArticles = async () => {
+//     try {
+//       setIsLoading(true);
+//       const res = await fetch("https://dev.to/api/articles?top=1&per_page=4");
+//       const data = await res.json();
+//       setTrendArticle(data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       setIsLoading(false);
+//       toast.warning("Интернет холболтоо шалгана уу");
+//     }
+//   };
+//   useEffect(() => {
+//     trendArticles();
+//   }, []);
+//   return (
+//     <section className="max-w-[1200px] m-auto mb-8">
+//       <h1 className="font-bold text-2xl mb-8">Trending</h1>
+//       <div className="flex gap-6 w-full">
+//         {trendArticle
+//           ?.filter((el) => el?.title.toLowerCase().includes(searchValue))
+//           .map(({ cover_image, title, type_of, id }, idx) => (
+//             <TrendPostCard
+//               key={idx}
+//               imgUrl={cover_image}
+//               tPostText={title}
+//               buttonTitle={type_of}
+//               id={id}
+//             />
+//           ))}
+//         {isLoading && <Loader />}
+//       </div>
+//     </section>
+//   );
+// };
+// export default TrendingPost;
+
 import TrendPostCard from "./tPostCard";
 import { useState, useEffect, useContext } from "react";
 import { MyContext } from "@/context/context";
 import { toast } from "react-toastify";
 import Loader from "../loader/loader";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 const TrendingPost = () => {
   const { searchValue } = useContext(MyContext);
   const [trendArticle, setTrendArticle] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
   const trendArticles = async () => {
     try {
       setIsLoading(true);
-      const res = await fetch("https://dev.to/api/articles?top=1&per_page=4");
+      const res = await fetch("https://dev.to/api/articles?top=1&per_page=6");
       const data = await res.json();
       setTrendArticle(data);
       setIsLoading(false);
@@ -24,9 +75,9 @@ const TrendingPost = () => {
     trendArticles();
   }, []);
   return (
-    <section className="max-w-[1200px] m-auto mb-8">
+    <section className="max-w-[1200px] embla m-auto mb-8" ref={emblaRef}>
       <h1 className="font-bold text-2xl mb-8">Trending</h1>
-      <div className="flex gap-6 w-full">
+      <div className="embla__container flex items-center px-10 overflow-scroll h-[400px] gap-6 w-full">
         {trendArticle
           ?.filter((el) => el?.title.toLowerCase().includes(searchValue))
           .map(({ cover_image, title, type_of, id }, idx) => (
